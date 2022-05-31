987ead903aea1c7da5af53c9dd4e133e8c3f3f13
Arsh <arsh@Assassins.venom.007>
2022-05-31 23:26:08 +0530

    Foreach

--- Day 3 - task/Resume.js
+++ Day 3 - task/Resume.js
@@ -1,10 +1,10 @@
-let resume= {
+let resume= [{
 Name:"Mohamed Arsath",
 Education: "B.E Mechanical",
 Work_Experience:"Software Tester (2021 - Present)",
-Technical_Skilss:[Html5, Css3, JavaScript, Python, Mongodb, React, Npm, Nodejs],
+Technical_Skilss:["Html5", "Css3", "JavaScript", "Python", "Mongodb", "React","Npm", "Nodejs"],
 Email: "Arshathm4@gmail.com",
-Phone_NO:9790380324,
+Phone_NO:9790380324},{
 Address:[{
     Door_No:"58/13",
     Street:"2nd Umar",
@@ -19,4 +19,8 @@ Address:[{
     Pincode:609703 }],
 Interest:["Martial Arts","Reading Books", "Bodybuilding"],
 Courses_and_Trainings: ["Web Design","Full stack development"]
-}
+}]
+
+resume.forEach((e, index) => {
+    console.log(e, index)
+});
